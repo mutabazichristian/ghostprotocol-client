314e0220b009716fc5bcde8657b8dff07a4a31c7
mutabazichristian <c.mutabazi@alustudent.com>
2023-12-01 22:53:57 +0200

    letsgo

--- src/API/index.js
+++ src/API/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const server= 'http://localhost:8081'
+// const server= 'http://localhost:8081'
+const server = 'https://jolly-vestments-dove.cyclic.app/'
 
 const instance = axios.create({
     baseURL: `${server}`
